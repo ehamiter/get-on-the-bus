a636e37786b02a2595b2551865e1f169aff29714
Eric Hamiter <ehamiter@moontoast.com>
2014-08-29 16:07:51 -0500

    Refactor variables and shorten frequency checking

--- alarm.py
+++ alarm.py
@@ -18,7 +18,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Configuration
-FREQUENCY_CHECK = 10 # in seconds
+FREQUENCY_CHECK = 5 # in seconds
 MP3_FOLDER = 'mp3s'
 
 system = platform.system().lower()
@@ -42,26 +42,26 @@ def calendar_event_query():
     events = service.events().list(singleEvents=True, calendarId=CALENDAR_ID).execute()
 
     for i, event in enumerate(events['items']):
-        event_name = event['summary'].lower()
-        event_start = event['start']['dateTime'][:-9]
-        event_description = event.get('description', '')
-        repeat = True if event_description.lower() == 'repeat' else False
+        name = event['summary'].lower()
+        start = event['start']['dateTime'][:-9]
+        description = event.get('description', '')
+        repeat = True if description.lower() == 'repeat' else False
         now = today.strftime('%Y-%m-%dT%H:%M')
 
-        if event_start >= now:
-            logger.debug('Event #%s, Event Name: %s, Event Start: %s', i, event_name, event_start)
+        if start >= now:
+            logger.debug('Event #%s, Name: %s, Start: %s', i, name, start)
 
-            if event_start == now:
-                if event_name.startswith('say'):
-                    event_name = re.sub(r'[^a-zA-Z0-9\s\']', '', event_name)
-                    command = '{0} "{1}"'.format('say' if system == 'darwin' else 'espeak -ven+m2', event_name[4:])
-                    logger.info('Event starting. Announcing \'%s\'...', event_name[4:])
+            if start == now:
+                if name.startswith('say'):
+                    name = re.sub(r'[^a-zA-Z0-9\s\']', '', name)
+                    command = '{0} "{1}"'.format('say' if system == 'darwin' else 'espeak -ven+m2', name[4:])
+                    logger.info('Event starting. Announcing \'%s\'...', name[4:])
                 else:
                     mp3_files = os.listdir(MP3_FOLDER)
-                    mp3_name = event_name.replace(' ', '_') + '.mp3'
+                    mp3_name = name.replace(' ', '_') + '.mp3'
                     mp3_name = mp3_name if mp3_name in mp3_files else 'default.mp3'
                     command = 'mpg123 \'{}/{}\''.format(MP3_FOLDER, mp3_name)
-                    logger.info('Event %s starting. Playing mp3 file %s...', event_name, mp3_name)
+                    logger.info('Event %s starting. Playing mp3 file %s...', name, mp3_name)
                 os.system(command)
                 if repeat == False:
                     time.sleep(60)
